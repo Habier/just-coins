e901e1022689e47164a33da2403ab276133cb5bd
Habier <Habier@users.noreply.github.com>
2017-11-30 00:06:39 +0100

    fixed server crash ó_ò

--- src/main/java/com/javigar/justcoins/JustCoins.java
+++ src/main/java/com/javigar/justcoins/JustCoins.java
@@ -7,11 +7,13 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = JustCoins.MODID, version = JustCoins.VERSION)
 public class JustCoins {
     public static final String MODID = "justcoins";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
 
 
     public static Item copperCoin;
@@ -27,6 +29,12 @@ public class JustCoins {
         goldCoin = new ItemBase("gold_coin");
         coinSack = new ItemBase("coin_sack");
 
+        if (event.getSide() == Side.CLIENT)
+            modelInits();
+    }
+
+    @SideOnly(Side.CLIENT)
+    private void modelInits() {
         ((IModeledItem) copperCoin).initModel();
         ((IModeledItem) silverCoin).initModel();
         ((IModeledItem) goldCoin).initModel();
