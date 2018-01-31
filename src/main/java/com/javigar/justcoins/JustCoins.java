482785e3fdc77e6b9962e7d00a221f1d6639d368
Habier <Habier@users.noreply.github.com>
2018-01-31 23:37:36 +0100

    note: 8 means 8, not 1.

--- src/main/java/com/javigar/justcoins/JustCoins.java
+++ src/main/java/com/javigar/justcoins/JustCoins.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @Mod(modid = JustCoins.MODID, version = JustCoins.VERSION)
 public class JustCoins {
     public static final String MODID = "justcoins";
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.1.4";
 
 
     public static Item copperCoin;
